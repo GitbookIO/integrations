d4e52bf38334ca737aac6fac77e2b6390933841a
Samy Pessé <samypesse@gmail.com>
2022-06-03 14:06:51 +0200

    Generate types for runtime-v8-isolated

--- packages/runtime-v8-isolated/build.sh
+++ packages/runtime-v8-isolated/build.sh
@@ -2,3 +2,4 @@
 
 rm -rf ./dist/
 esbuild ./src/index.ts  --platform=node --outfile=./dist/index.js --log-level=warning
+tsc --project tsconfig.json --declaration --allowJs --emitDeclarationOnly --outDir ./dist/
