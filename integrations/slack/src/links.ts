71b8d614b4efe3f5fa7d9c9d194637952116cb53
Samy Pessé <samypesse@gmail.com>
2022-07-10 01:53:35 +0200

    Continue

--- integrations/slack/src/links.ts
+++ integrations/slack/src/links.ts
@@ -1,3 +1,4 @@
+import * as gitbook from '@gitbook/api';
 import { api } from '@gitbook/runtime';
 
 import { executeSlackAPIRequest } from './api';
@@ -24,6 +25,7 @@ interface LinkSharedSlackEvent {
  * Unfurl a link on Slack with metadata from GitBook.
  */
 export async function unfurlLink(event: LinkSharedSlackEvent) {
+    // Lookup the concerned installations
     const {
         data: { items: installations },
     } = await api.integrations.listIntegrationInstallations('slack', {
@@ -32,52 +34,46 @@ export async function unfurlLink(event: LinkSharedSlackEvent) {
 
     console.log('found', installations.length, 'installations');
 
-    const unfurls = {};
+    const installation = installations[0];
+    if (!installation) {
+        return {};
+    }
 
+    // Authentify as the installation
+    const { data: installationToken } = await api.integrations.createIntegrationInstallationToken(
+        'slack',
+        installation.id
+    );
+    const installationApiClient = new gitbook.GitBookAPI({
+        authToken: installationToken.token,
+    });
+
+    // Resolve links to their content
+    const unfurls = {};
     await Promise.all(
         event.event.links.map(async (link) => {
-            console.log('unfurl link', link.url);
+            console.log('unfurl link', link.url, encodeURIComponent(link.url));
 
-            unfurls[link.url] = {
-                blocks: [
-                    {
-                        type: 'header',
-                        text: {
-                            type: 'plain_text',
-                            text: 'Title of the space',
-                            emoji: true,
-                        },
-                    },
-                    {
-                        type: 'section',
-                        fields: [
-                            {
-                                type: 'mrkdwn',
-                                text: '*Last updated:*\nAug 10',
-                            },
-                            {
-                                type: 'mrkdwn',
-                                text: '*Created by:*\n<example.com|Fred Enriquez>',
-                            },
-                        ],
-                        accessory: {
-                            type: 'button',
-                            text: {
-                                type: 'plain_text',
-                                text: 'Click Me',
-                                emoji: true,
-                            },
-                            value: 'click_me_123',
-                            action_id: 'button-action',
-                        },
-                    },
-                ],
-            };
+            const { data: content } = await installationApiClient.urls.getContentByUrl({
+                url: link.url,
+            });
+
+            console.log('got', content);
+
+            if (!content) {
+                return;
+            }
+
+            if ('space' in content && content.space) {
+                unfurls[link.url] = createBlocksForSpace(content.space);
+            }
+            if ('collection' in content && content.collection) {
+                unfurls[link.url] = createBlocksForCollection(content.collection);
+            }
         })
     );
 
-    const installation = installations[0];
-
+    // Send the unfurls to Slack
     await executeSlackAPIRequest(
         'POST',
         'chat.unfurl',
@@ -92,4 +88,67 @@ export async function unfurlLink(event: LinkSharedSlackEvent) {
     );
 
     return {};
+}
+
+function createBlocksForSpace(space: gitbook.Space) {
+    return {
+        blocks: [
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text: `*<${space.urls.app}|${space.title || 'Space'}>*`,
+                },
+            },
+            {
+                type: 'section',
+                fields: [
+                    {
+                        type: 'mrkdwn',
+                        text: `*Visibility:*\${space.visibility}`,
+                    },
+                    {
+                        type: 'mrkdwn',
+                        text: `*Last updated:*\n${space.updatedAt}`,
+                    },
+                ],
+                accessory: {
+                    type: 'button',
+                    text: {
+                        type: 'plain_text',
+                        text: 'Open in GitBook',
+                        emoji: true,
+                    },
+                    value: 'open',
+                    url: space.urls.app,
+                    action_id: 'button-action',
+                },
+            },
+        ],
+    };
+}
+
+function createBlocksForCollection(collection: gitbook.Collection) {
+    return {
+        blocks: [
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text: `*<${collection.urls.app}|${collection.title || 'Collection'}>*`,
+                },
+                accessory: {
+                    type: 'button',
+                    text: {
+                        type: 'plain_text',
+                        text: 'Open in GitBook',
+                        emoji: true,
+                    },
+                    value: 'open',
+                    url: collection.urls.app,
+                    action_id: 'button-action',
+                },
+            },
+        ],
+    };
 }
