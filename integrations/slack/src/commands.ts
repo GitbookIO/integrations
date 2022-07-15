50fef5a9874526aa58b120f972759d7c88caa50c
taranvohra <taranvohra@outlook.com>
2022-07-15 11:20:04 +0530

    /gitbook slash command

--- integrations/slack/src/commands.ts
+++ integrations/slack/src/commands.ts
@@ -24,12 +24,13 @@ export interface SlashEvent {
 }
 
 export function createSlackCommandsHandler(handlers: {
-    [type: string]: (slashEvent: SlashEvent) => Promise<any>;
+    [type: string]: (event: SlashEvent) => Promise<any>;
 }): (request: Request) => Promise<Response> {
     return async (request) => {
-        const slashEvent = (await request.formData()) as SlashEvent;
-
-        console.log('received slash command', JSON.stringify(slashEvent, null, 2));
+        const slashEvent = {} as SlashEvent;
+        new URLSearchParams(await request.text()).forEach((value, key) => {
+            slashEvent[key] = value;
+        });
 
         if (!slashEvent.command) {
             return new Response(`Invalid slash command`, {
@@ -46,20 +47,10 @@ export function createSlackCommandsHandler(handlers: {
             });
         }
 
-        const data = await handler(slashEvent);
+        handler(slashEvent);
 
-        if (typeof data === 'string') {
-            return new Response(data, {
-                headers: {
-                    'Content-Type': 'text/plain',
-                },
-            });
-        }
-
-        return new Response(JSON.stringify(data), {
-            headers: {
-                'Content-Type': 'application/json',
-            },
+        return new Response(null, {
+            status: 200,
         });
     };
 }
--- integrations/slack/src/search.ts
+++ integrations/slack/src/search.ts
@@ -1,4 +1,7 @@
+import { SearchPageResult, SearchSectionResult, SearchSpaceResult } from '@gitbook/api';
 import { api } from '@gitbook/runtime';
+
+import { executeSlackAPIRequest } from './api';
 
 import type { SlashEvent } from './commands';
 
@@ -29,5 +32,70 @@ export async function searchInGitBook(slashEvent: SlashEvent) {
 
     const {
         data: { items },
-    } = await installationApiClient.search.searchContent({ query: text, limit: 10 });
+    } = await installationApiClient.search.searchContent({ query: text, limit: 5 });
+
+    const blocks = buildSearchContentSlackBlocks(items);
+
+    await executeSlackAPIRequest(
+        'POST',
+        'chat.postMessage',
+        { channel: channel_id, blocks, unfurl_links: false, unfurl_media: false },
+        {
+            accessToken: installation.configuration.oauth_credentials?.access_token,
+        }
+    );
+}
+
+function buildSearchContentSlackBlocks(items: SearchSpaceResultt[]) {
+    const allPages = items.flatMap((space) => space.pages);
+    const blocks = allPages.reduce((acc, page) => {
+        const pageResultBlock = buildSearchPageBlock(page);
+        if (page.sections) {
+            const sectionBlocks = page.sections.map(buildSearchSectionBlock);
+            acc.push(...pageResultBlock, ...sectionBlocks);
+        }
+        return acc;
+    }, []);
+
+    return blocks.flat();
+}
+
+function buildSearchPageBlock(page: SearchPageResult) {
+    return [
+        {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: `*<https://app.gitbook.com/${page.path}|${page.title}>*`,
+            },
+        },
+        {
+            type: 'divider',
+        },
+    ];
+}
+
+function buildSearchSectionBlock(section: SearchSectionResult) {
+    const title = `*${section.title.replace(/"/g, '')}*`;
+    const body = `_${section.body.replace(/"/g, '').split('\n').join('').slice(0, 512)}_`;
+    const text = `${title} - ${body}`;
+    return [
+        {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text,
+            },
+            accessory: {
+                type: 'button',
+                text: {
+                    type: 'plain_text',
+                    text: 'View',
+                    emoji: true,
+                },
+                ...(section.path ? { url: `https://app.gitbook.com/${section.path}` } : {}),
+                action_id: section.id,
+            },
+        },
+    ];
 }
