a1b68ddc4918b567f334d7480327891ae8fadeac
Samy Pessé <samypesse@gmail.com>
2022-07-18 22:20:10 +0200

    Prioritize environment variable when setting configuration value

--- packages/cli/src/config.ts
+++ packages/cli/src/config.ts
@@ -2,10 +2,7 @@ import Conf from 'conf';
 
 import { GITBOOK_DEFAULT_ENDPOINT } from '@gitbook/api';
 
-/**
- * Config preserved in the user's home directory.
- */
-export const config = new Conf<{
+interface CliConfig {
     /**
      * Endpoint for the API.
      */
@@ -14,10 +11,35 @@ export const config = new Conf<{
      * Token to use for authentication.
      */
     token?: string;
-}>({
+}
+
+/**
+ * Config preserved in the user's home directory.
+ */
+const config = new Conf<CliConfig>({
     projectName: 'gitbook',
     defaults: {
         endpoint: process.env.GITBOOK_ENDPOINT || GITBOOK_DEFAULT_ENDPOINT,
         token: process.env.GITBOOK_TOKEN,
     },
 });
+
+/**
+ * Get a value from the config file, but prioritize the environment variable.
+ */
+export function getConfigValue(key: keyof CliConfig): string | undefined {
+    const envName = `GITBOOK_${key.toUpperCase()}`;
+    const envValue = process.env[envName];
+    if (envValue !== undefined) {
+        return envValue;
+    }
+
+    return config.get(key);
+}
+
+/**
+ * Set a value in the config file.
+ */
+export function setConfigValue<Key extends keyof CliConfig>(key: Key, value: CliConfig[Key]): void {
+    config.set(key, value);
+}
