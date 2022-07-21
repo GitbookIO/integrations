a46cbc34fda73e1068ec917bbf22dc05f2160b8c
taranvohra <taranvohra@outlook.com>
2022-07-21 12:21:59 +0530

    slack signature verify

---
+++ integrations/slack/src/middlewares.ts
@@ -0,0 +1,43 @@
+import { Request } from 'itty-router';
+import { sha256 } from 'js-sha256';
+
+export async function verifySlackRequest(req: Request) {
+    // @ts-ignore
+    const slackSignature = req.headers.get('x-slack-signature');
+    // @ts-ignore
+    const timestamp = req.headers.get('x-slack-request-timestamp');
+
+    const body = (await req.text()) as string;
+    const baseSignature = `v0:${timestamp}:${body}`;
+
+    const computedSignature = `v0=${sha256.hmac
+        .create('6c4cfa4389178a9e7625612a544d5a2b')
+        .update(baseSignature)
+        .hex()}`;
+
+    if (!secureCompare(slackSignature, computedSignature)) {
+        throw new Error('Invalid signature');
+    }
+}
+
+/**
+ * Secure compare two strings to prevent timing attacks.
+ * Implementation taken from: https://github.com/vadimdemedes/secure-compare
+ * TODO: Once the `crypto` is implemented in the runtime, we can use `crypto.timingSafeEqual`
+ */
+function secureCompare(a: string, b: string): boolean {
+    if (typeof a !== 'string' || typeof b !== 'string') {
+        return false;
+    }
+
+    let mismatch = a.length === b.length ? 0 : 1;
+    if (mismatch) {
+        b = a;
+    }
+
+    for (let i = 0; i < a.length; ++i) {
+        mismatch |= a.charCodeAt(i) ^ b.charCodeAt(i);
+    }
+
+    return mismatch === 0;
+}
