16fb206b30b433f734705085ef770bd7ad9934f7
Taran Vohra <taranvohra@outlook.com>
2022-07-25 13:34:17 +0530

    Acknowledge slack req immediately (#46)
    * acknowledge slack req immediately
    
    * shift code
    
    * move comment

--- integrations/slack/src/middlewares.ts
+++ integrations/slack/src/middlewares.ts
@@ -31,6 +31,26 @@ export async function verifySlackRequest(req: Request) {
 }
 
 /**
+ * We acknowledge the slack request immediately to avoid failures
+ * and "queue" the actual task to be executed in a subsequent request.
+ */
+export async function acknowledgeSlackRequest(req: Request) {
+    fetch(`${req.url}_task`, {
+        method: 'POST',
+        body: await req.text(),
+        headers: {
+            'content-type': req.headers.get('content-type'),
+            'x-slack-signature': req.headers.get('x-slack-signature'),
+            'x-slack-request-timestamp': req.headers.get('x-slack-request-timestamp'),
+        },
+    });
+
+    return new Response(JSON.stringify({ acknowledged: true }), {
+        'Content-Type': 'application/json',
+    });
+}
+
+/**
  * Secure compare two strings to prevent timing attacks.
  * Implementation taken from: https://github.com/vadimdemedes/secure-compare
  * TODO: Once the `crypto` is implemented in the runtime, we can use `crypto.timingSafeEqual`
