933543f704ee36657a2fa455e6d2f5e332b6ea68
Samy Pessé <samypesse@gmail.com>
2022-10-07 22:50:15 +0200

    Start

---
+++ integrations/arcade/src/index.tsx
@@ -0,0 +1,98 @@
+import {
+    createIntegration,
+    createComponent,
+    RuntimeEnvironment,
+    RuntimeContext,
+} from '@gitbook/runtime';
+
+interface ArcadeInstallationConfiguration {}
+
+type ArcadeRuntimeEnvironment = RuntimeEnvironment<ArcadeInstallationConfiguration>;
+type ArcadeRuntimeContext = RuntimeContext<ArcadeRuntimeEnvironment>;
+
+/**
+ * Component to render the block when embeding an Arcade URL.
+ */
+const embedBlock = createComponent<{
+    flowId?: string;
+    url?: string;
+}>({
+    componentId: 'embed',
+
+    async action(element, action) {
+        switch (action.action) {
+            case '@link.unfurl': {
+                const { url } = action;
+                const nodeProps = extractFlowFromURL(url);
+
+                return {
+                    props: {
+                        ...nodeProps,
+                        url,
+                    },
+                };
+            }
+        }
+
+        return element;
+    },
+
+    async render(element, context) {
+        const { flowId, url } = element.props;
+
+        if (!flowId) {
+            return (
+                <block>
+                    <card
+                        title={'Not found'}
+                        onPress={{
+                            action: '@ui.url.open',
+                            url,
+                        }}
+                        icon={
+                            <image
+                                source={{
+                                    url: context.environment.integration.urls.icon,
+                                }}
+                                aspectRatio={1}
+                            />
+                        }
+                    />
+                </block>
+            );
+        }
+
+        // https://app.arcade.software/api/oembed?url=https://app.arcade.software/share/DTXVNC1rbhyoL9kTn1pD
+
+        return (
+            <block>
+                <webframe
+                    source={{
+                        url: `https://demo.arcade.software/${flowId}`,
+                    }}
+                    aspectRatio={1024 / 571}
+                />
+            </block>
+        );
+    },
+});
+
+function extractFlowFromURL(input: string): {
+    flowId?: string;
+} {
+    const url = new URL(input);
+    if (url.hostname !== 'app.arcade.software') {
+        return;
+    }
+
+    const parts = url.pathname.split('/');
+    if (parts[1] !== 'flows') {
+        return;
+    }
+
+    return { flowId: parts[2] };
+}
+
+export default createIntegration<ArcadeRuntimeContext>({
+    components: [embedBlock],
+});
