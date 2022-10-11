d1a25f5e2ae5b9cf2a926dd21d9dd7c563cfe99c
Valentino Hudhra <v.hudhra@gmail.com>
2022-10-11 11:59:51 +0200

    fix package.json syntax issue

--- packages/runtime/jsx-dev-runtime.d.ts
+++ packages/runtime/jsx-dev-runtime.d.ts
@@ -6,7 +6,7 @@ import { jsx, jsxDEV, jsxs, Fragment } from './src/contentkit-jsx';
  * https://github.com/microsoft/TypeScript/issues/33079
  */
 
-declare module '@gitbook/runtime/jsx-runtime' {
+declare module '@gitbook/runtime/jsx-dev-runtime' {
     export type jsx = typeof jsx;
     export type jsxs = typeof jsxs;
     export type jsxDEV = typeof jsxDEV;
