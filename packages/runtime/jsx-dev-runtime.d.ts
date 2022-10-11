350711ca0c152a5102b89b4fb6af5f87663500d8
Valentino Hudhra <v.hudhra@gmail.com>
2022-10-11 08:41:45 +0200

    Add comments

--- packages/runtime/jsx-dev-runtime.d.ts
+++ packages/runtime/jsx-dev-runtime.d.ts
@@ -1,6 +1,11 @@
 // eslint-disable-next-line import/no-internal-modules
 import { jsx, jsxDEV, jsxs, Fragment } from './src/contentkit-jsx';
 
+/**
+ * This is a workaround for Typescript not supporting subpath exports in package.json
+ * https://github.com/microsoft/TypeScript/issues/33079
+ */
+
 declare module '@gitbook/runtime/jsx-runtime' {
     export type jsx = typeof jsx;
     export type jsxs = typeof jsxs;
