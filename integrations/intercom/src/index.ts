121caebbbf5662f8a01085b11041602057e0177a
Guillaume <guillaume@gitbook.io>
2022-10-13 15:45:59 -0400

    Update intercom to make sure it does work

--- integrations/intercom/src/index.ts
+++ integrations/intercom/src/index.ts
@@ -29,39 +29,58 @@ export const handleFetchEvent: FetchPublishScriptEventCallback = async (
         `
 var APP_ID = "${appId}";
 
-(function(){
-    var w = window;
-    var ic = w.Intercom;
-    if (typeof ic === "function") {
+window.intercomSettings = {
+    app_id: APP_ID
+};
+
+(function () {
+    const w = window;
+    const ic = w.Intercom;
+    if (typeof ic === 'function') {
         ic('reattach_activator');
-        ic('update',w.intercomSettings);
+        ic('update', w.intercomSettings);
     } else {
-        var d = document;
-        var i = function(){i.c(arguments);};
+        const d = document;
+        const i = function() {
+            i.c(arguments);
+        };
         i.q = [];
-        i.c = function(args){i.q.push(args);};
+        i.c = function (args) {
+            i.q.push(args);
+        };
         w.Intercom = i;
-        var l = function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/' + APP_ID;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);};
-        if (document.readyState==='complete') {
+        const l = function () {
+            const s = d.createElement('script');
+            s.type = 'text/javascript';
+            s.async = true;
+            s.src = 'https://widget.intercom.io/widget/${appId}';
+            s.onload = () => {
+                ic('boot', {
+                    app_id: APP_ID,
+                });
+            };
+
+            const x = d.getElementsByTagName('script')[0];
+            x.parentNode.insertBefore(s, x);
+
+            w.GitBook.addEventListener('unload', () => {
+                if (!ic) {
+                    return;
+                }
+
+                ic('shutdown');
+                w.Intercom = undefined;
+            });
+        };
+        if (document.readyState === 'complete') {
             l();
         } else if (w.attachEvent) {
-            w.attachEvent('onload',l);
+            w.attachEvent('onload', l);
         } else {
-            w.addEventListener('load',l,false);
+            w.addEventListener('load', l, false);
         }
     }
-    
-    window.Gitbook.addEventListener('unload', () => {
-        if (!ic) {
-            return;
-        }
-
-        ic('shutdown');
-        w.Intercom = undefined;
-    });
 })();
-            
-
 `,
         {
             headers: {
