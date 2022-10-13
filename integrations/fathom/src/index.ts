da8698d8372d9b0ce0996208c33182f8d92b6eaf
Guillaume <guillaume@gitbook.io>
2022-10-13 14:27:37 -0400

    Update package-lock.json after resolving teh conflicts

---
+++ integrations/fathom/src/index.ts
@@ -0,0 +1,42 @@
+import {
+    createIntegration,
+    FetchPublishScriptEventCallback,
+    RuntimeContext,
+    RuntimeEnvironment,
+} from '@gitbook/runtime';
+
+import script from './plausibleScript.raw.js';
+
+type FathomRuntimeContext = RuntimeContext<
+    RuntimeEnvironment<
+        {},
+        {
+            site_id?: string;
+        }
+    >
+>;
+
+export const handleFetchEvent: FetchPublishScriptEventCallback = async (
+    event,
+    { environment }: FathomRuntimeContext
+) => {
+    const siteId = environment.spaceInstallation.configuration.site_id;
+    if (!siteId) {
+        throw new Error(
+            `The Fathom site ID is missing from the Space (ID: ${event.installationId}) installation.`
+        );
+    }
+
+    return new Response(script.replace('<TO_REPLACE>', siteId), {
+        headers: {
+            'Content-Type': 'application/javascript',
+            'Cache-Control': 'max-age=604800',
+        },
+    });
+};
+
+export default createIntegration<FathomRuntimeContext>({
+    events: {
+        fetch_published_script: handleFetchEvent,
+    },
+});
