b480afe0dfca169b064313c7dc4b6323966396c0
Samy Pessé <samypesse@gmail.com>
2023-01-16 18:22:15 +0100

    Cleanup

--- integrations/plausible/src/plausibleScript.raw.js
+++ integrations/plausible/src/plausibleScript.raw.js
@@ -1,15 +1,14 @@
-(function (doc, domain) {
+(function (doc, domain, api) {
     const sibling = doc.getElementsByTagName('script')[0];
     const element = doc.createElement('script');
 
     element.defer = true;
     element.setAttribute('data-domain', domain);
 
-    const api = '<api>';
     if (api) {
         element.setAttribute('data-api', api);
     }
 
     element.src = 'https://plausible.io/js/script.js';
     sibling.parentNode.insertBefore(element, sibling);
-})(document, '<domain>');
+})(document, '<domain>', '<api>');
