1c1e82ea1f32c24c4ca66bdf74ad327c29c4bd4e
megantipps <megan@thedelta.io>
2023-05-23 15:58:20 +0200

    initial sandpack commit

---
+++ integrations/sandpack/src/index.tsx
@@ -0,0 +1,45 @@
+import {
+    createIntegration,
+    createComponent,
+    FetchEventCallback,
+    RuntimeContext,
+} from '@gitbook/runtime';
+
+type IntegrationContext = {} & RuntimeContext;
+
+const handleFetchEvent: FetchEventCallback<IntegrationContext> = async (request, context) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { api } = context;
+    const user = api.user.getAuthenticatedUser();
+
+    return new Response(JSON.stringify(user));
+};
+
+const exampleBlock = createComponent({
+    componentId: 'sandpack',
+    initialState: (props) => {
+        return {
+            message: 'Click Me',
+        };
+    },
+    action: async (element, action, context) => {
+        switch (action.action) {
+            case 'click':
+                console.log('Button Clicked');
+                return {};
+        }
+    },
+    render: async (element, action, context) => {
+        return (
+            <block>
+                <button label={element.state.message} onPress={{ action: 'click' }} />
+            </block>
+        );
+    },
+});
+
+export default createIntegration({
+    fetch: handleFetchEvent,
+    components: [exampleBlock],
+    events: {},
+});
