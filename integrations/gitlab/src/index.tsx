7a7f6c9450760a43a54785a5c4fde896a3cc4bce
chanlevandermerwe <chanlevdmerwe@gmail.com>
2023-05-24 12:04:34 +0100

    Revert "feat(gitlab-unfurl): authentication updates"
    This reverts commit 6579d14b883f8528d645c67f4156036e8f437d33.

--- integrations/gitlab/src/index.tsx
+++ integrations/gitlab/src/index.tsx
@@ -1,14 +1,4 @@
-import { Router } from 'itty-router';
-
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { RequestUpdateIntegrationInstallation } from '@gitbook/api';
-import {
-    createIntegration,
-    createComponent,
-    createOAuthHandler,
-    OAuthResponse,
-    FetchEventCallback,
-} from '@gitbook/runtime';
+import { createIntegration, createComponent, createOAuthHandler } from '@gitbook/runtime';
 
 import { getGitlabContent, GitlabProps } from './gitlab';
 import { GitlabRuntimeContext } from './types';
@@ -93,62 +83,17 @@ const gitlabCodeBlock = createComponent<{ url?: string }, {}, {}, GitlabRuntimeC
     },
 });
 
-const handleFetchEvent: FetchEventCallback<GitlabRuntimeContext> = async (request, context) => {
-    const { environment } = context;
-
-    const router = Router({
-        base: new URL(
-            environment.spaceInstallation?.urls?.publicEndpoint ||
-                environment.installation?.urls.publicEndpoint ||
-                environment.integration.urls.publicEndpoint
-        ).pathname,
-    });
-
-    /*
-     * Authenticate the user using OAuth.
-     */
-    router.get(
-        '/oauth',
-        // @ts-ignore
-        createOAuthHandler({
-            redirectURL: `${context.environment.integration.urls.publicEndpoint}/oauth`,
-            clientId: environment.secrets.CLIENT_ID,
-            clientSecret: environment.secrets.CLIENT_SECRET,
-            authorizeURL: 'https://gitlab.com/login/oauth/authorize',
-            accessTokenURL: 'https://gitlab.com/login/oauth/access_token',
-            scopes: ['repo'],
-            prompt: 'consent',
-            extractCredentials,
-        })
-    );
-
-    const response = await router.handle(request, context);
-    if (!response) {
-        return new Response(`No route matching ${request.method} ${request.url}`, {
-            status: 404,
-        });
-    }
-
-    return response;
-};
-
-const extractCredentials = async (
-    response: OAuthResponse
-): Promise<RequestUpdateIntegrationInstallation> => {
-    const { access_token } = response;
-
-    return {
-        configuration: {
-            oauth_credentials: {
-                access_token,
-                expires_at: Date.now() + response.expires_in * 1000,
-                refresh_token: response.refresh_token,
-            },
-        },
-    };
-};
-
 export default createIntegration<GitlabRuntimeContext>({
-    fetch: handleFetchEvent,
+    fetch: (request, context) => {
+        const oauthHandler = createOAuthHandler({
+            redirectURL: `${context.environment.integration.urls.publicEndpoint}/oauth`,
+            clientId: context.environment.secrets.CLIENT_ID,
+            clientSecret: context.environment.secrets.CLIENT_SECRET,
+            authorizeURL: 'https://gitlab.com/login/oauth/authorize?scope=repo',
+            accessTokenURL: 'https://gitlab.com/login/oauth/access_token',
+        });
+
+        return oauthHandler(request, context);
+    },
     components: [gitlabCodeBlock],
 });
