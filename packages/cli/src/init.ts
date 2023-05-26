5b563edb6c38bc8407634978dfe30a2147f2de9d
Addison <addison@gitbook.com>
2023-05-26 15:16:49 +0900

    Fix type errors

--- packages/cli/src/init.ts
+++ packages/cli/src/init.ts
@@ -196,6 +196,9 @@ export function generateScript(project: { name: string }): string {
       } from "@gitbook/runtime";
       
       type IntegrationContext = {} & RuntimeContext;
+      type IntegrationBlockProps = {};
+      type IntegrationBlockState = { message: string };
+      type IntegrationAction = { action: "click" };
       
       const handleFetchEvent: FetchEventCallback<IntegrationContext> = async (
         request,
@@ -208,14 +211,19 @@ export function generateScript(project: { name: string }): string {
         return new Response(JSON.stringify(user));
       };
       
-      const exampleBlock = createComponent({
+      const exampleBlock = createComponent<
+         IntegrationBlockProps,
+         IntegrationBlockState,
+         IntegrationAction,
+         IntegrationContext
+      >({
         componentId: "${project.name}",
         initialState: (props) => {
           return {
             message: "Click Me",
           };
         },
-        action: async (element, action: { action: string }, context) => {
+        action: async (element, action, context) => {
           switch (action.action) {
             case "click":
               console.log("Button Clicked");
