088f850c0fce84aad97daa0478d91364e9456605
Chanlé van der Merwe <87859354+chana-banana@users.noreply.github.com>
2023-05-31 12:29:46 +0200

    GitLab unfurl integration (#155)
    * initial commit for gitlab
    
    * updated gitlab start project
    
    * feat(gitlab-unfurl): integration setup
    
    * feat(gitlab-unfurl): extract information from permalink
    
    * feat(gitlab-unfurl): get content
    
    * feat(gitlab-unfurl): multi-line, single line and whole file unfurling
    
    * feat(gitlab-unfurl): break out into smaller steps
    
    * feat(gitlab-unfurl): break out into separate files
    
    * feat(gitlab-unfurl): remove comments
    
    * feat(gitlab-unfurl): update packages
    
    * feat(gitlab-unfurl): add private repo validation
    
    * feat(gitlab-unfurl): authentication updates
    
    * Revert "feat(gitlab-unfurl): authentication updates"
    
    This reverts commit 6579d14b883f8528d645c67f4156036e8f437d33.
    
    * feat(gitlab-unfurl): update urls
    
    * added verifier and code challenge
    
    * updated code from crypto to web crypto format
    
    * added 32 random character string as state to the authorise url
    
    * updated authorise url and update some authorisation token logic
    
    * added the api scope to the authorise url
    
    * updated the authorise headers
    
    * feat(gitlab-unfurl): remove plain text parameter from permalinks
    
    * feat(gitlab-unfurl): make visibility public
    
    * Update metadata, republish
    
    * Add preview image, republish
    
    ---------
    
    Co-authored-by: megantipps <megan@thedelta.io>
    Co-authored-by: Addison <addison@gitbook.com>
    Co-authored-by: Addison <42930383+addisonschultz@users.noreply.github.com>

---
+++ integrations/gitlab/src/index.tsx
@@ -0,0 +1,99 @@
+import { createIntegration, createComponent, createOAuthHandler } from '@gitbook/runtime';
+
+import { getGitlabContent, GitlabProps } from './gitlab';
+import { GitlabRuntimeContext } from './types';
+
+const gitlabCodeBlock = createComponent<{ url?: string }, {}, {}, GitlabRuntimeContext>({
+    componentId: 'gitlab-code-block',
+    async action(element, action) {
+        switch (action.action) {
+            case '@link.unfurl': {
+                const { url } = action;
+
+                return {
+                    props: {
+                        url,
+                    },
+                };
+            }
+        }
+
+        return element;
+    },
+    async render(element, context) {
+        const { url } = element.props as GitlabProps;
+        const content = await getGitlabContent(url, context);
+
+        if (!content) {
+            return (
+                <block>
+                    <card
+                        title={'Not found'}
+                        onPress={{
+                            action: '@ui.url.open',
+                            url,
+                        }}
+                        icon={
+                            <image
+                                source={{
+                                    url: context.environment.integration.urls.icon,
+                                }}
+                                aspectRatio={1}
+                            />
+                        }
+                    />
+                </block>
+            );
+        }
+
+        return (
+            <block>
+                <card
+                    title={url}
+                    onPress={{
+                        action: '@ui.url.open',
+                        url,
+                    }}
+                    icon={
+                        <image
+                            source={{
+                                url: context.environment.integration.urls.icon,
+                            }}
+                            aspectRatio={1}
+                        />
+                    }
+                    buttons={[
+                        <button
+                            icon="maximize"
+                            tooltip="Open preview"
+                            onPress={{
+                                action: '@ui.modal.open',
+                                componentId: 'previewModal',
+                                props: {
+                                    url,
+                                },
+                            }}
+                        />,
+                    ]}
+                >
+                    {content ? <codeblock content={content.toString()} lineNumbers={true} /> : null}
+                </card>
+            </block>
+        );
+    },
+});
+
+export default createIntegration<GitlabRuntimeContext>({
+    fetch: (request, context) => {
+        const oauthHandler = createOAuthHandler({
+            redirectURL: `${context.environment.integration.urls.publicEndpoint}/oauth`,
+            clientId: context.environment.secrets.CLIENT_ID,
+            clientSecret: context.environment.secrets.CLIENT_SECRET,
+            authorizeURL: 'https://gitlab.com/oauth/authorize?scope=read_repository+api',
+            accessTokenURL: 'https://gitlab.com/oauth/token',
+        });
+
+        return oauthHandler(request, context);
+    },
+    components: [gitlabCodeBlock],
+});
