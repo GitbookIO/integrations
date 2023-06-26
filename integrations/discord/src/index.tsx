71cf45ee4ebad5f0c83f1080073f4397e51aa76b
Addison <42930383+addisonschultz@users.noreply.github.com>
2023-06-26 16:20:31 +0200

    Discord Notifications (#188)
    * feat(discord): initial commit
    
    * feat(discord): initial auth setup
    
    * added a scope for discord to allow channel selection
    
    * added a scope for discord to allow channel selection
    
    * added space content updated event to discord integration
    
    * added messages api url to discord integration
    
    * feat(discord): get channel id via auth webhook
    
    * added webhook to extended oauthresponse
    
    * feat(discord): use bot token for auth
    
    * feat(discord): post message in channel and add embedObject
    
    * feat(discord): organise and clean up todos and comments
    
    * Update manifest
    
    ---------
    
    Co-authored-by: chanlevandermerwe <chanlevdmerwe@gmail.com>
    Co-authored-by: megantipps <megan@thedelta.io>

---
+++ integrations/discord/src/index.tsx
@@ -0,0 +1,84 @@
+import { Router } from 'itty-router';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { RequestUpdateIntegrationInstallation } from '@gitbook/api';
+import {
+    createIntegration,
+    createOAuthHandler,
+    FetchEventCallback,
+    EventCallback,
+} from '@gitbook/runtime';
+
+import { sendDiscordMessage } from './discord';
+import { DiscordRuntimeContext, OAuthResponseWebhook } from './types';
+
+/*
+ * Handle content being updated: send a notification on Slack.
+ */
+const handleSpaceContentUpdated: EventCallback<
+    'space_content_updated',
+    DiscordRuntimeContext
+> = async (event, context) => {
+    await sendDiscordMessage(event, context);
+};
+
+const handleFetchEvent: FetchEventCallback<DiscordRuntimeContext> = async (request, context) => {
+    const { environment } = context;
+
+    const router = Router({
+        base: new URL(
+            environment.spaceInstallation?.urls?.publicEndpoint ||
+                environment.installation?.urls.publicEndpoint ||
+                environment.integration.urls.publicEndpoint
+        ).pathname,
+    });
+
+    /*
+     * Authenticate the user using OAuth.
+     */
+    router.get(
+        '/oauth',
+        // @ts-ignore
+        createOAuthHandler({
+            redirectURL: `${context.environment.integration.urls.publicEndpoint}/oauth`,
+            clientId: environment.secrets.CLIENT_ID,
+            clientSecret: environment.secrets.CLIENT_SECRET,
+            authorizeURL:
+                'https://discord.com/api/oauth2/authorize?response_type=code&permissions=2147485696',
+            accessTokenURL: 'https://discord.com/api/oauth2/token',
+            scopes: ['applications.commands', 'bot', 'webhook.incoming'],
+            extractCredentials,
+        })
+    );
+
+    const response = await router.handle(request, context);
+    if (!response) {
+        return new Response(`No route matching ${request.method} ${request.url}`, {
+            status: 404,
+        });
+    }
+    return response;
+};
+
+const extractCredentials = async (
+    response: OAuthResponseWebhook
+): Promise<RequestUpdateIntegrationInstallation> => {
+    const { access_token, webhook } = response;
+    return {
+        configuration: {
+            oauth_credentials: {
+                access_token,
+                expires_at: Date.now() + response.expires_in * 1000,
+                refresh_token: response.refresh_token,
+                webhook,
+            },
+        },
+    };
+};
+
+export default createIntegration<DiscordRuntimeContext>({
+    fetch: handleFetchEvent,
+    events: {
+        space_content_updated: handleSpaceContentUpdated,
+    },
+});
