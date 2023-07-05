1de6f2f8c3194be1e40499ec3ec68abd6eb6d89b
Addison <42930383+addisonschultz@users.noreply.github.com>
2023-07-05 18:13:07 +0200

    Make Formspree Integration more functional (#192)
    * Make Formspree Integration more functional
    
    * Default email true, fix form submission

--- integrations/formspree/src/utils.ts
+++ integrations/formspree/src/utils.ts
@@ -1,9 +1,11 @@
 // Handle errors better
 
-export async function handleSubmit(formspree_id, email) {
+export async function handleSubmit(formspree_id, body) {
+    const cleanedFormBody = await removeEmptyValues(body);
+
     fetch(formspree_id, {
         method: 'POST',
-        body: JSON.stringify({ email, form: 'GitBook Integration' }),
+        body: JSON.stringify({ data: cleanedFormBody, form: 'GitBook Integration' }),
         headers: {
             Accept: 'application/json',
         },
@@ -18,4 +20,17 @@ export async function handleSubmit(formspree_id, email) {
         .catch((error) => {
             console.error(error);
         });
+}
+
+// Clean data object being submitted
+export async function removeEmptyValues(object) {
+    for (const key in object) {
+        if (object.hasOwnProperty(key)) {
+            const value = object[key];
+            if (value === null || value === undefined || value === '') {
+                delete object[key];
+            }
+        }
+    }
+    return object;
 }
