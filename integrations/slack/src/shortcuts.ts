663e3066c4c6c04ebbc53c695dcfa784b0258a3f
Valentino Hudhra <v.hudhra@gmail.com>
2023-08-17 13:45:20 +0200

    move comments around

--- integrations/slack/src/shortcuts.ts
+++ integrations/slack/src/shortcuts.ts
@@ -25,43 +25,6 @@ export function createSlackShortcutsHandler(
         console.log('shortcutPayload', shortcutPayload);
         const { channel, message, team, user, response_url } = shortcutPayload;
 
-        //   token: '1W4r987ZaX2XbnrkVvfdEzQQ',
-        //   action_ts: '1692187902.226777',
-        //   team: { id: 'T032HV6MF', domain: 'gitbook' },
-        //   user: {
-        //     id: 'U03S41KSY8M',
-        //     username: 'valentino',
-        //     team_id: 'T032HV6MF',
-        //     name: 'valentino'
-        //   },
-        //   channel: { id: 'C05M1K6RTD4', name: 'tmp-slack-integration-valentino' },
-        //   is_enterprise_install: false,
-        //   enterprise: null,
-        //   callback_id: 'generate_docs',
-        //   trigger_id: '5749554069538.3085992729.84e5c8b2c3f206a0a64417a02cb6270d',
-        //   response_url: 'https://hooks.slack.com/app/T032HV6MF/5735015787015/6zzBIWRLpw6sm438oUpTxC2h',
-        //   message_ts: '1692106894.378009',
-        //   message: {
-        //     bot_id: 'B05MND68QVC',
-        //     type: 'message',
-        //     text: 'Hm, I had the case where it would hang at this point, but it was related to changes to our mySQL database and the CLI was waiting for a prompt answer (y/n) higher in the logs.\n' +
-        //       'Can you check if you have anything earlier that could be blocking too?',
-        //     user: 'U03S41KSY8M',
-        //     ts: '1692106894.378009',
-        //     app_id: 'A05M82VRGPM',
-        //     blocks: [ [Object] ],
-        //     team: 'T032HV6MF',
-        //     bot_profile: {
-        //       id: 'B05MND68QVC',
-        //       deleted: false,
-        //       name: 'GitBook (valentino-dev)',
-        //       updated: 1692103834,
-        //       app_id: 'A05M82VRGPM',
-        //       icons: [Object],
-        //       team_id: 'T032HV6MF'
-        //     },
-        //     thread_ts: '1692106894.088119',
-        //     parent_user_id: 'U03S41KSY8M'
         // console.log('event', event);
 
         // await addRecording(api, event.event, environment.secrets.BOT_TOKEN);
@@ -71,19 +34,6 @@ export function createSlackShortcutsHandler(
             channel: channel.id,
             thread_ts: message.thread_ts,
         });
-
-        // await fetch(response_url, {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify({
-        //         text: `Have a look, docs created ${recordThreadRes.url}`,
-        //         replace_original: false,
-        //         thread_ts: message.thread_ts,
-        //         response_type: 'in_channel',
-        //     }),
-        // });
 
         const res = await slackAPI(
             context,
@@ -159,34 +109,43 @@ export function createSlackShortcutsHandler(
         return new Response(null, {
             status: 200,
         });
-
-        // const eventType = event.event?.type || event.type;
-        // // Find the handle for the event type, or use the fallback if that's missing
-        // const handler = handlers[eventType];
-        // if (!handler) {
-        //     if (fallback) {
-        //         return fallback(request, context);
-        //     }
-
-        //     return new Response(`No handler for event type "${eventType}"`, {
-        //         status: 404,
-        //     });
-        // }
-
-        // const data = await handler(event, context);
-
-        // if (typeof data === 'string') {
-        //     return new Response(data, {
-        //         headers: {
-        //             'Content-Type': 'text/plain',
-        //         },
-        //     });
-        // }
-
-        // return new Response(JSON.stringify(data), {
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        // });
     };
 }
+
+//   token: '1W4r987ZaX2XbnrkVvfdEzQQ',
+//   action_ts: '1692187902.226777',
+//   team: { id: 'T032HV6MF', domain: 'gitbook' },
+//   user: {
+//     id: 'U03S41KSY8M',
+//     username: 'valentino',
+//     team_id: 'T032HV6MF',
+//     name: 'valentino'
+//   },
+//   channel: { id: 'C05M1K6RTD4', name: 'tmp-slack-integration-valentino' },
+//   is_enterprise_install: false,
+//   enterprise: null,
+//   callback_id: 'generate_docs',
+//   trigger_id: '5749554069538.3085992729.84e5c8b2c3f206a0a64417a02cb6270d',
+//   response_url: 'https://hooks.slack.com/app/T032HV6MF/5735015787015/6zzBIWRLpw6sm438oUpTxC2h',
+//   message_ts: '1692106894.378009',
+//   message: {
+//     bot_id: 'B05MND68QVC',
+//     type: 'message',
+//     text: 'Hm, I had the case where it would hang at this point, but it was related to changes to our mySQL database and the CLI was waiting for a prompt answer (y/n) higher in the logs.\n' +
+//       'Can you check if you have anything earlier that could be blocking too?',
+//     user: 'U03S41KSY8M',
+//     ts: '1692106894.378009',
+//     app_id: 'A05M82VRGPM',
+//     blocks: [ [Object] ],
+//     team: 'T032HV6MF',
+//     bot_profile: {
+//       id: 'B05MND68QVC',
+//       deleted: false,
+//       name: 'GitBook (valentino-dev)',
+//       updated: 1692103834,
+//       app_id: 'A05M82VRGPM',
+//       icons: [Object],
+//       team_id: 'T032HV6MF'
+//     },
+//     thread_ts: '1692106894.088119',
+//     parent_user_id: 'U03S41KSY8M'
