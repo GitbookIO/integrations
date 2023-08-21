4e6d41c2b39ec32a4a23702988a9c0feaf8ebcaf
Valentino Hudhra <v.hudhra@gmail.com>
2023-08-21 08:25:58 +0200

    fix syntax

--- integrations/slack/src/ui/blocks/index.ts
+++ integrations/slack/src/ui/blocks/index.ts
@@ -125,7 +125,7 @@ export function FollowUpQueryList(props: { queries: Array<string> }) {
     //     type: 'button',
     //     text: {
     //         type: 'plain_text',
-           text: question.slice(0, 75),
+    //    text: question.slice(0, 75),
     //         emoji: true,
     //     },
     //     value: 'queryLens',
