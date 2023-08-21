9b0418a63a87aa8e7ab0cca8ccb3ceefe5e2809e
Valentino Hudhra <v.hudhra@gmail.com>
2023-08-21 23:50:58 +0200

    don't unfurl

--- integrations/slack/src/actions/queryLens.ts
+++ integrations/slack/src/actions/queryLens.ts
@@ -139,6 +139,7 @@ export async function queryLens({
                     ...(threadId ? { thread_ts: threadId } : {}),
                     blocks,
                     user: userId,
+                    unfurl_links: false,
                 },
             };
         }
