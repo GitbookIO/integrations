350bc23af5b44a72b3bca60d84f487b7c3c30f4c
Taran Vohra <taranvohra@outlook.com>
2023-08-31 20:30:58 +0530

    GitHub Integration (#214)
    * wip
    
    * api module
    
    * wip
    
    * cleanup types
    
    * fix manifest
    
    * fix infinite loop
    
    * save config
    
    * provider apis
    
    * trigger methods
    
    * fix ref in export
    
    * webhooks
    
    * webhooks
    
    * fix comments
    
    * handle space content updated
    
    * git sync events
    
    * import export with force
    
    * FIXME
    
    * fix types
    
    * add gitbook/api dep
    
    * refactor actions
    
    * update parse methods signature
    
    * use jose to sign a github app jwt
    
    * refactor using github apis
    
    * fix webhooks
    
    * fix comment
    
    * fix types
    
    * http errors
    
    * remove catch handler in webhook
    
    * refactor name to event
    
    * compute version hash
    
    * refactor
    
    * compute commit message
    
    * refactor
    
    * improve hint text, add comments
    
    * @tsndr/cloudflare-worker-jwt
    
    * use jose to sign jwt, fml
    
    * wip
    
    * wip refactor
    
    * refactor
    
    * webhook task
    
    * refactor
    
    * fix api call
    
    * fix bugs
    
    * fix endpoints for reconciliation
    
    * fix webhook
    
    * refactor
    
    * save config
    
    * refactor
    
    * accept input prop
    
    * add image to installation account options
    
    * use waitUntil
    
    * merge main
    
    * add refresh logic
    
    * bug fixes
    
    * bug fix webhooks
    
    * update CI yaml

---
+++ integrations/github/src/api.ts
@@ -0,0 +1,326 @@
+import httpError from 'http-errors';
+import LinkHeader from 'http-link-header';
+
+import { Logger } from '@gitbook/runtime';
+
+import type { GithubRuntimeContext, GitHubSpaceConfiguration } from './types';
+import { assertIsDefined, getSpaceConfigOrThrow } from './utils';
+
+export type OAuthTokenCredentials = NonNullable<GitHubSpaceConfiguration['oauth_credentials']>;
+
+const logger = Logger('github:api');
+
+/**
+ * NOTE: These GH types are not complete, they are just what we need for now.
+ */
+
+interface GHInstallation {
+    id: number;
+    account: {
+        id: number;
+        login: string;
+        avatar_url: string;
+    };
+}
+
+interface GHRepository {
+    id: number;
+    name: string;
+    full_name: string;
+    owner: {
+        id: number;
+        login: string;
+    };
+    visibility: 'public' | 'private';
+}
+
+interface GHBranch {
+    name: string;
+    protected: boolean;
+}
+
+/**
+ * Fetch all installations for the current GitHub authentication. It will use
+ * the access token from the environment.
+ */
+export async function fetchInstallations(context: GithubRuntimeContext) {
+    const installations = await githubAPI<Array<GHInstallation>>(context, null, {
+        path: '/user/installations',
+        params: {
+            per_page: 100,
+            page: 1,
+        },
+        listProperty: 'installations',
+    });
+
+    return installations;
+}
+
+/**
+ * Fetch all repositories for a given installation.
+ */
+export async function fetchInstallationRepositories(
+    context: GithubRuntimeContext,
+    installationId: number
+) {
+    const repositories = await githubAPI<Array<GHRepository>>(context, null, {
+        path: `/user/installations/${installationId}/repositories`,
+        params: {
+            per_page: 100,
+            page: 1,
+        },
+        listProperty: 'repositories',
+    });
+
+    return repositories;
+}
+
+/**
+ * Fetch a repository by its ID.
+ */
+export async function fetchRepository(context: GithubRuntimeContext, repositoryId: number) {
+    const repository = await githubAPI<GHRepository>(context, null, {
+        path: `/repositories/${repositoryId}`,
+    });
+
+    return repository;
+}
+
+/**
+ * Fetch all branches for a given account repository.
+ */
+export async function fetchRepositoryBranches(context: GithubRuntimeContext, repositoryId: number) {
+    const branches = await githubAPI<Array<GHBranch>>(context, null, {
+        path: `/repositories/${repositoryId}/branches`,
+        params: {
+            per_page: 100,
+            page: 1,
+        },
+    });
+
+    return branches;
+}
+
+/**
+ * Get an access token for the GitHub App installation
+ * using the GitHub App JWT.
+ */
+export async function createAppInstallationAccessToken(
+    context: GithubRuntimeContext,
+    appJWT: string,
+    installationId: number
+): Promise<string> {
+    const { token } = await githubAPI<{ token: string }>(
+        context,
+        { access_token: appJWT, expires_at: 0 },
+        {
+            method: 'POST',
+            path: `/app/installations/${installationId}/access_tokens`,
+        }
+    );
+
+    return token;
+}
+
+/**
+ * Create a commit status for a commit SHA using the GitHub
+ * installation access token.
+ */
+export async function createCommitStatus(
+    context: GithubRuntimeContext,
+    installationAccessToken: string,
+    owner: string,
+    repo: string,
+    sha: string,
+    status: object
+): Promise<void> {
+    await githubAPI(
+        context,
+        { access_token: installationAccessToken, expires_at: 0 },
+        {
+            method: 'POST',
+            path: `/repos/${owner}/${repo}/statuses/${sha}`,
+            body: status,
+        }
+    );
+}
+
+/**
+ * Execute a GitHub API request.
+ */
+async function githubAPI<T>(
+    context: GithubRuntimeContext,
+    // Will be extracted from context if null
+    tokenCredentials: OAuthTokenCredentials | null,
+    request: {
+        method?: 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE';
+        path: string;
+        body?: object;
+        params?: object;
+        /** Property to get an array for pagination */
+        listProperty?: string;
+    }
+): Promise<T> {
+    const { method = 'GET', path, body, params, listProperty = '' } = request;
+
+    const credentials = tokenCredentials || extractTokenCredentialsOrThrow(context);
+
+    const url = new URL(`https://api.github.com${path}`);
+    Object.entries(params || {}).forEach(([key, value]) => {
+        url.searchParams.set(key, value);
+    });
+    const options = {
+        method,
+        body: body ? JSON.stringify(body) : undefined,
+    };
+
+    const response = await requestGitHubAPI(context, credentials, url, options);
+
+    let data = await response.json();
+
+    let paginatedListProperty = false;
+    if (listProperty) {
+        // @ts-ignore
+        data = data[listProperty];
+        paginatedListProperty = true;
+    }
+
+    // Pagination
+    let res = response;
+    while (res.headers.has('Link')) {
+        const link = LinkHeader.parse(res.headers.get('Link') || '');
+        if (link.has('rel', 'next')) {
+            const nextURL = new URL(link.get('rel', 'next')[0].uri);
+            const nextURLSearchParams = Object.fromEntries(nextURL.searchParams);
+            if (nextURLSearchParams.page) {
+                url.searchParams.set('page', nextURLSearchParams.page as string);
+                const nextResponse = await requestGitHubAPI(context, credentials, url, options);
+                const nextData = await nextResponse.json();
+                // @ts-ignore
+                data = [...data, ...(paginatedListProperty ? nextData[listProperty] : nextData)];
+                res = nextResponse;
+            }
+        } else {
+            break;
+        }
+    }
+
+    return data as T;
+}
+
+/**
+ * Make a fetch request to the GitHub API using the given token credentials.
+ * It will throw an error if the response is not ok.
+ */
+async function requestGitHubAPI(
+    context: GithubRuntimeContext,
+    credentials: OAuthTokenCredentials,
+    url: URL,
+    options: RequestInit = {},
+    retriesLeft = 1
+): Promise<Response> {
+    const { access_token } = credentials;
+    const response = await fetch(url.toString(), {
+        ...options,
+        headers: {
+            ...options.headers,
+            Accept: 'application/vnd.github+json',
+            Authorization: `Bearer ${access_token}`,
+            'User-Agent': 'GitHub-Integration-Worker',
+            'X-GitHub-Api-Version': '2022-11-28',
+        },
+    });
+
+    if (!response.ok) {
+        // If the access token is expired, we try to refresh it
+        if (response.status === 401 && retriesLeft > 0 && credentials?.refresh_token) {
+            const spaceInstallation = context.environment.spaceInstallation;
+            assertIsDefined(spaceInstallation, { label: 'spaceInstallation' });
+
+            logger.debug(`refreshing OAuth credentials for space ${spaceInstallation.space}`);
+
+            const refreshed = await refreshCredentials(
+                context.environment.secrets.CLIENT_ID,
+                context.environment.secrets.CLIENT_SECRET,
+                credentials.refresh_token
+            );
+
+            await context.api.integrations.updateIntegrationSpaceInstallation(
+                spaceInstallation.integration,
+                spaceInstallation.installation,
+                spaceInstallation.space,
+                {
+                    configuration: {
+                        ...spaceInstallation.configuration,
+                        oauth_credentials: refreshed,
+                    },
+                }
+            );
+
+            logger.info(`refreshed OAuth credentials for space ${spaceInstallation.space}`);
+
+            // Retry the request with the refreshed credentials
+            return requestGitHubAPI(context, refreshed, url, options, retriesLeft - 1);
+        }
+
+        // Otherwise, we throw an error
+        throw httpError(response.status, `GitHub API error: ${response.statusText}`);
+    }
+
+    return response;
+}
+
+async function refreshCredentials(
+    clientId: string,
+    clientSecret: string,
+    refreshToken: string
+): Promise<OAuthTokenCredentials> {
+    const url = new URL('https://github.com/login/oauth/access_token');
+
+    url.searchParams.set('client_id', clientId);
+    url.searchParams.set('client_secret', clientSecret);
+    url.searchParams.set('grant_type', 'refresh_token');
+    url.searchParams.set('refresh_token', refreshToken);
+
+    const resp = await fetch(url.toString(), {
+        method: 'POST',
+        headers: {
+            'User-Agent': 'GitHub-Integration-Worker',
+        },
+    });
+
+    if (!resp.ok) {
+        // If refresh fails for whatever reason, we ask the user to re-authenticate
+        throw httpError(401, `Unauthorized: kindly re-authenticate!`);
+    }
+
+    const data = await resp.formData();
+
+    return {
+        access_token: data.get('access_token') as string,
+        refresh_token: data.get('refresh_token') as string,
+        expires_at: Math.floor(Date.now() / 1000) + parseInt(data.get('expires_in') as string, 10),
+    };
+}
+
+/**
+ * Extract the token credentials of the space installation configuration from the context
+ * This will throw an error if the access token is not defined.
+ */
+export function extractTokenCredentialsOrThrow(
+    context: GithubRuntimeContext
+): OAuthTokenCredentials {
+    const spaceInstallation = context.environment.spaceInstallation;
+    assertIsDefined(spaceInstallation, {
+        label: 'spaceInstallation',
+    });
+
+    const config = getSpaceConfigOrThrow(spaceInstallation);
+
+    const oAuthCredentials = config?.oauth_credentials;
+    if (!oAuthCredentials?.access_token) {
+        throw httpError(401, 'Unauthorized: kindly re-authenticate!');
+    }
+
+    return oAuthCredentials;
+}
---
+++ integrations/github/src/provider.ts
@@ -0,0 +1,136 @@
+import * as jose from 'jose';
+
+import { GitSyncOperationState } from '@gitbook/api';
+import { Logger } from '@gitbook/runtime';
+
+import { createAppInstallationAccessToken, createCommitStatus } from './api';
+import { GithubRuntimeContext, GitHubSpaceConfiguration } from './types';
+import { assertIsDefined, parseInstallationOrThrow } from './utils';
+
+const logger = Logger('github:provider');
+
+/**
+ * Return the GitHub App JWT signed with the private key.
+ */
+async function getGitHubAppJWT(context: GithubRuntimeContext): Promise<string> {
+    const { environment } = context;
+
+    const now = Math.floor(Date.now() / 1000);
+
+    const appId = environment.secrets.APP_ID;
+    const pkcs8 = environment.secrets.PRIVATE_KEY;
+
+    const alg = 'RS256';
+
+    const privateKey = await jose.importPKCS8(pkcs8, alg);
+
+    const jwt = await new jose.SignJWT({
+        iat: now - 60,
+        exp: now + 60 * 10,
+        iss: appId,
+    })
+        .setProtectedHeader({ alg })
+        .sign(privateKey);
+
+    return jwt;
+}
+
+/**
+ * Returns the URL of the Git repository.
+ */
+export function getRepositoryUrl(config: GitHubSpaceConfiguration, withExtension = false): string {
+    assertIsDefined(config.accountName, { label: 'config.accountName' });
+    assertIsDefined(config.repoName, { label: 'config.repoName' });
+
+    return `https://github.com/${config.accountName}/${config.repoName}${
+        withExtension ? '.git' : ''
+    }`;
+}
+
+/**
+ * Returns the authentication information for the Git repository.
+ */
+export async function getRepositoryAuth(
+    context: GithubRuntimeContext,
+    config: GitHubSpaceConfiguration
+) {
+    const appJWT = await getGitHubAppJWT(context);
+    const installationId = parseInstallationOrThrow(config);
+    const installationAccessToken = await createAppInstallationAccessToken(
+        context,
+        appJWT,
+        installationId
+    );
+
+    return {
+        url: getRepositoryUrl(config, true),
+        username: 'x-access-token',
+        password: installationAccessToken,
+    };
+}
+
+/**
+ * Update the commit status
+ */
+export async function updateCommitStatus(
+    context: GithubRuntimeContext,
+    config: GitHubSpaceConfiguration,
+    commitSha: string,
+    update: {
+        context?: string;
+        state: GitSyncOperationState;
+        url: string;
+        description: string;
+    }
+) {
+    assertIsDefined(config.accountName, { label: 'config.accountName' });
+    assertIsDefined(config.repoName, { label: 'config.repoName' });
+
+    const appJWT = await getGitHubAppJWT(context);
+    const installationAccessToken = await createAppInstallationAccessToken(
+        context,
+        appJWT,
+        parseInstallationOrThrow(config)
+    );
+
+    await createCommitStatus(
+        context,
+        installationAccessToken,
+        config.accountName,
+        config.repoName,
+        commitSha,
+        {
+            state: update.state === 'running' ? 'pending' : update.state,
+            target_url: update.url,
+            description: update.description,
+            context: update.context || 'GitBook',
+        }
+    );
+
+    logger.info(
+        `Commit status updated for ${commitSha} on GitHub repo (${config.accountName}/${config.repoName})`
+    );
+}
+
+/**
+ * Returns the base URL of the Git tree in the provider.
+ */
+export function getGitTreeURL(config: GitHubSpaceConfiguration): string {
+    const base = getRepositoryUrl(config);
+    return `${base}/blob/${config.branch}`;
+}
+
+/**
+ * Returns the absolute URL for a commit.
+ */
+export function getGitCommitURL(config: GitHubSpaceConfiguration): string {
+    const base = getRepositoryUrl(config);
+    return `${base}/commit`;
+}
+
+/**
+ * Returns the Git ref to use for the synchronization.
+ */
+export function getGitRef(branch: string): string {
+    return `refs/heads/${branch}`;
+}
