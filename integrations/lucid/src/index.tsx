80e401e2c676ba8ac349cfa3be9818af387a8d73
Addison <addison@gitbook.com>
2024-01-16 12:39:51 +0100

    Add embedded iFrame

--- integrations/lucid/src/index.tsx
+++ integrations/lucid/src/index.tsx
@@ -1,9 +1,24 @@
-import { createIntegration, createComponent, createOAuthHandler } from '@gitbook/runtime';
+import { Router } from 'itty-router';
+
+import {
+    createIntegration,
+    createComponent,
+    createOAuthHandler,
+    FetchEventCallback,
+} from '@gitbook/runtime';
+
+const defaultContent = '';
 
 const LucidComponent = createComponent<{
     url?: string;
+    content?: string;
 }>({
     componentId: 'lucid-component',
+    initialState: (props) => {
+        return {
+            content: props.content || defaultContent,
+        };
+    },
 
     async action(element, action) {
         switch (action.action) {
@@ -27,33 +42,80 @@ const LucidComponent = createComponent<{
         const urlObject = new URL(url);
         const documentId = urlObject.pathname.split('/')[2];
 
-        const lucidEmbedUrl = `https://lucid.app/embeds/link?document=${documentId}&clientId=${context.environment.secrets.CLIENT_ID}`;
+        const frameUrl = new URL(context.environment.spaceInstallation?.urls?.publicEndpoint);
+        frameUrl.searchParams.set('document', documentId);
 
-        return (
-            <block>
-                <webframe
-                    source={{
-                        url: lucidEmbedUrl,
-                    }}
-                    aspectRatio={1}
-                />
-            </block>
+        const output = (
+            <webframe
+                source={{
+                    url: frameUrl.toString(),
+                }}
+                aspectRatio={16 / 9}
+            />
         );
+
+        return <block>{output}</block>;
     },
 });
 
-export default createIntegration({
-    fetch: (request, context) => {
-        const oauthHandler = createOAuthHandler({
+const handleFetchEvent: FetchEventCallback = async (request, context) => {
+    const { environment } = context;
+
+    const router = Router({
+        base: new URL(
+            environment.spaceInstallation?.urls?.publicEndpoint ||
+                environment.installation?.urls.publicEndpoint ||
+                environment.integration.urls.publicEndpoint
+        ).pathname,
+    });
+
+    router.get('/', async () => {
+        return new Response(
+            `<html>
+                <body>
+                    <iframe id="lucid-embed" src="https://lucid.app/embeds/link?document=${new URL(
+                        request.url
+                    ).searchParams.get('document')}&clientId=${
+                context.environment.secrets.CLIENT_ID
+            }" style="height:100%; width: 100%;" frameborder="0" border="0"></iframe>
+                </body>
+            </html>`,
+            {
+                headers: {
+                    'Content-Type': 'text/html',
+                    'Cache-Control': 'public, max-age=86400',
+                },
+            }
+        );
+    });
+
+    /*
+     * Authenticate the user using OAuth.
+     */
+    router.get(
+        '/oauth',
+        // @ts-ignore
+        createOAuthHandler({
             redirectURL: `${context.environment.integration.urls.publicEndpoint}/oauth`,
             clientId: context.environment.secrets.CLIENT_ID,
             clientSecret: context.environment.secrets.CLIENT_SECRET,
             scopes: ['lucidchart.document.app'],
             authorizeURL: 'https://lucid.app/oauth2/authorize',
             accessTokenURL: `https://api.lucid.co/oauth2/token`,
-        });
+        })
+    );
 
-        return oauthHandler(request, context);
-    },
+    const response = await router.handle(request, context);
+    if (!response) {
+        return new Response(`No route matching ${request.method} ${request.url}`, {
+            status: 404,
+        });
+    }
+
+    return response;
+};
+
+export default createIntegration({
+    fetch: handleFetchEvent,
     components: [LucidComponent],
 });
