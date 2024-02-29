6623cef93c6337b5ac8e7b0b53c1b2c755c70519
Vib Bhardwaj <bhardwajvibhanshu+github@gmail.com>
2024-02-29 14:48:26 +0000

    Add Mixpanel integration

---
+++ integrations/mixpanel/src/index.ts
@@ -0,0 +1,40 @@
+import {
+    createIntegration,
+    FetchPublishScriptEventCallback,
+    RuntimeContext,
+    RuntimeEnvironment,
+} from '@gitbook/runtime';
+
+import script from './script.raw.js';
+
+type MixpanelRuntimeContext = RuntimeContext<
+    RuntimeEnvironment<
+        {},
+        {
+            project_token?: string;
+        }
+    >
+>;
+
+export const handleFetchEvent: FetchPublishScriptEventCallback = async (
+    event,
+    { environment }: MixpanelRuntimeContext
+) => {
+    const projectToken = environment.spaceInstallation.configuration.project_token;
+    if (!projectToken) {
+        throw new Error(
+            `The Mixpanel Project Token is missing from the configuration (ID: ${event.spaceId}).`
+        );
+    }
+
+    return new Response(script.replace('<TO_REPLACE>', projectToken), {
+        headers: {
+            'Content-Type': 'application/javascript',
+            'Cache-Control': 'max-age=604800',
+        },
+    });
+};
+
+export default createIntegration<MixpanelRuntimeContext>({
+    fetch_published_script: handleFetchEvent,
+});
