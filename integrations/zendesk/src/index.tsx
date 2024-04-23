be81de1ead77a59fd4db14c68046fff04d767935
Addison <addison@gitbook.com>
2024-04-23 09:14:52 +0900

    Add boilerplate for zendesk integration

---
+++ integrations/zendesk/src/index.tsx
@@ -0,0 +1,44 @@
+import {
+    createIntegration,
+    FetchPublishScriptEventCallback,
+    RuntimeContext,
+    RuntimeEnvironment,
+} from '@gitbook/runtime';
+
+import script from './script.raw.js';
+
+type ZendeskRuntimeContext = RuntimeContext<
+    RuntimeEnvironment<
+        {},
+        {
+            site_tag?: string;
+        }
+    >
+>;
+
+export const handleFetchEvent: FetchPublishScriptEventCallback = async (
+    event,
+    { environment }: ZendeskRuntimeContext
+) => {
+    const trackingId =
+        environment.siteInstallation?.configuration?.site_tag ??
+        environment.spaceInstallation.configuration.site_tag;
+    if (!trackingId) {
+        throw new Error(
+            `The Zendesk site tag is missing from the configuration (ID: ${
+                'spaceId' in event ? event.spaceId : event.siteId
+            }).`
+        );
+    }
+
+    return new Response(script.replace('<TO_REPLACE>', trackingId), {
+        headers: {
+            'Content-Type': 'application/javascript',
+            'Cache-Control': 'max-age=604800',
+        },
+    });
+};
+
+export default createIntegration<ZendeskRuntimeContext>({
+    fetch_published_script: handleFetchEvent,
+});
---
+++ integrations/zendesk/src/script.raw.js
@@ -0,0 +1,14 @@
+(function (h, o, t, j, a, r) {
+    const trackingID = '<TO_REPLACE>';
+    h.hj =
+        h.hj ||
+        function () {
+            (h.hj.q = h.hj.q || []).push(arguments);
+        };
+    h._hjSettings = { hjid: trackingID, hjsv: 6 };
+    a = o.getElementsByTagName('head')[0];
+    r = o.createElement('script');
+    r.async = 1;
+    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window, document, 'WIP', '.js?sv=');
