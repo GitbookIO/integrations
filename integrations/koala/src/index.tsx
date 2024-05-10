d7343bda41b413185a580f285b13397b5cfaddf4
Addison <addison@gitbook.com>
2024-05-10 12:08:36 +0900

    Update Koala manifest

--- integrations/koala/src/index.tsx
+++ integrations/koala/src/index.tsx
@@ -3,54 +3,51 @@ import {
     createComponent,
     FetchEventCallback,
     RuntimeContext,
-  } from "@gitbook/runtime";
+} from '@gitbook/runtime';
 
-  type IntegrationContext = {} & RuntimeContext;
-  type IntegrationBlockProps = {};
-  type IntegrationBlockState = { message: string };
-  type IntegrationAction = { action: "click" };
+type IntegrationContext = {} & RuntimeContext;
+type IntegrationBlockProps = {};
+type IntegrationBlockState = { message: string };
+type IntegrationAction = { action: 'click' };
 
-  const handleFetchEvent: FetchEventCallback<IntegrationContext> = async (
-    request,
-    context
-  ) => {
+const handleFetchEvent: FetchEventCallback<IntegrationContext> = async (request, context) => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { api } = context;
     const user = api.user.getAuthenticatedUser();
 
     return new Response(JSON.stringify(user));
-  };
+};
 
-  const exampleBlock = createComponent<
-     IntegrationBlockProps,
-     IntegrationBlockState,
-     IntegrationAction,
-     IntegrationContext
-  >({
-    componentId: "koala",
+const exampleBlock = createComponent<
+    IntegrationBlockProps,
+    IntegrationBlockState,
+    IntegrationAction,
+    IntegrationContext
+>({
+    componentId: 'koala',
     initialState: (props) => {
-      return {
-        message: "Click Me",
-      };
+        return {
+            message: 'Click Me',
+        };
     },
     action: async (element, action, context) => {
-      switch (action.action) {
-        case "click":
-          console.log("Button Clicked");
-          return {};
-      }
+        switch (action.action) {
+            case 'click':
+                console.log('Button Clicked');
+                return {};
+        }
     },
     render: async (element, context) => {
-      return (
-        <block>
-          <button label={element.state.message} onPress={{ action: "click" }} />
-        </block>
-      );
+        return (
+            <block>
+                <button label={element.state.message} onPress={{ action: 'click' }} />
+            </block>
+        );
     },
-  });
+});
 
-  export default createIntegration({
+export default createIntegration({
     fetch: handleFetchEvent,
     components: [exampleBlock],
     events: {},
-  });
+});
