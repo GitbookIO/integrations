467ba5984f949e6ce368a36fa4b96f4139652cd7
Addison <addison@gitbook.com>
2024-05-13 16:46:54 +0900

    Add tracking script

--- integrations/koala/src/script.raw.js
+++ integrations/koala/src/script.raw.js
@@ -1,3 +1,16 @@
-(function () {
-    const trackingID = '<TO_REPLACE>';
-})(window, document);
+(function (t) {
+    if (window.ko) return;
+    (window.ko = []),
+        ['identify', 'track', 'removeListeners', 'open', 'on', 'off', 'qualify', 'ready'].forEach(
+            function (t) {
+                ko[t] = function () {
+                    var n = [].slice.call(arguments);
+                    return n.unshift(t), ko.push(n), ko;
+                };
+            }
+        );
+    var n = document.createElement('script');
+    (n.async = !0),
+        n.setAttribute('src', 'https://cdn.getkoala.com/v1/<TO_REPLACE>/sdk.js'),
+        (document.body || document.head).appendChild(n);
+})();
