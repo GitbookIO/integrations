629603ee9f38bfb211e2a9e1ddc70b2cc3a9244a
Addison <addison@gitbook.com>
2024-06-20 14:32:21 +0900

    Add initial code for crisp integration

---
+++ integrations/crisp/src/script.raw.js
@@ -0,0 +1,17 @@
+// (function (h, o, t, j, a, r) {
+//     const trackingID = '<TO_REPLACE>';
+//     h.hj =
+//         h.hj ||
+//         function () {
+//             (h.hj.q = h.hj.q || []).push(arguments);
+//         };
+//     h._hjSettings = { hjid: trackingID, hjsv: 6 };
+//     a = o.getElementsByTagName('head')[0];
+//     r = o.createElement('script');
+//     r.async = 1;
+//     r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+//     a.appendChild(r);
+// })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+
+// Example script
+// <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="<TO_REPLACE>";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
