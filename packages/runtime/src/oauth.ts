1f869869601a26f43b34160b87944223ca1faca3
Valentino Hudhra <v.hudhra@gmail.com>
2024-10-03 15:32:07 +0200

    wip

--- packages/runtime/src/oauth.ts
+++ packages/runtime/src/oauth.ts
@@ -184,7 +184,6 @@ export function createOAuthHandler<TOAuthResponse = OAuthResponse>(
             // @ts-ignore
             const credentials = await extractCredentials(json);
             logger.debug(`exchange code for credentials`, credentials);
-            console.log(`exchange code for credentials`, credentials);
 
             const rawState = url.searchParams.get('state');
             if (!rawState) {
