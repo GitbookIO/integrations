8758229bf557177ab31f21aa42b448f36dedf245
Johan Preynat <johan.preynat@gmail.com>
2024-11-21 14:32:48 +0100

    Don't catch router errors to allow them to be caught in GitHub integration

--- integrations/github/src/index.ts
+++ integrations/github/src/index.ts
@@ -364,10 +364,7 @@ const handleFetchEvent: FetchEventCallback<GithubRuntimeContext> = async (reques
         });
     });
 
-    const response = (await router.handle(request, context).catch((err) => {
-        logger.error(`error handling request ${err.message} ${err.stack}`);
-        return error(err);
-    })) as Response | undefined;
+    const response = (await router.handle(request, context)) as Response | undefined;
 
     if (!response) {
         return new Response(`No route matching ${request.method} ${request.url}`, {
