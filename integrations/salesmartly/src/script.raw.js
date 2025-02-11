5c02fc2aea5bc37fc2cb55f952151b8d54a4d310
rick <100116396+rick20220222@users.noreply.github.com>
2025-02-11 15:10:45 +0800

    update

--- integrations/salesmartly/src/script.raw.js
+++ integrations/salesmartly/src/script.raw.js
@@ -5,18 +5,10 @@
 
     var d = document;
 
-    var l = function () {
-        var s = d.createElement('script');
-        s.type = 'text/javascript';
-        s.async = true;
-        s.src = 'https://assets.salesmartly.com/js/' + APP_ID + '.js';
-        var x = d.getElementsByTagName('script')[0];
-        x.parentNode.insertBefore(s, x);
-    };
-    if (w.attachEvent) {
-        w.attachEvent('onload', l);
-    } else {
-        w.addEventListener('load', l, false);
-    }
-
+    var s = d.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'https://assets.salesmartly.com/js/project_142743_159464_1729740328.js';
+    var x = d.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
 })();
