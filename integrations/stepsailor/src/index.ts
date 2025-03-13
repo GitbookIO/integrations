e169e0f18fdf0fa52d5edda624b615aef35bfeba
Pascal Lohscheidt <pascal.lohscheidt@gmail.com>
2025-03-13 14:35:36 +0100

    fixed stepsailor integration

--- integrations/stepsailor/src/index.ts
+++ integrations/stepsailor/src/index.ts
@@ -1,55 +1,52 @@
 import {
-  createIntegration,
-  FetchPublishScriptEventCallback,
-  RuntimeContext,
-  RuntimeEnvironment,
+    createIntegration,
+    FetchPublishScriptEventCallback,
+    RuntimeContext,
+    RuntimeEnvironment,
 } from '@gitbook/runtime';
 
 import script from './script.raw.js';
 
 type StepsailorRuntimeContext = RuntimeContext<
-  RuntimeEnvironment<
-    {},
-    {
-      organizationId?: string;
-      deployConfigId?: string;
-      apiKey?: string;
-    }
-  >
+    RuntimeEnvironment<
+        {},
+        {
+            organizationId?: string;
+            deployConfigId?: string;
+            apiKey?: string;
+        }
+    >
 >;
 
 export const handleFetchEvent: FetchPublishScriptEventCallback = async (
-  event,
-  { environment }: StepsailorRuntimeContext
+    event,
+    { environment }: StepsailorRuntimeContext,
 ) => {
-  const organizationId =
-    environment.siteInstallation?.configuration?.organizationId;
-  const deployConfigId =
-    environment.siteInstallation?.configuration?.deployConfigId;
-  const apiKey = environment.siteInstallation?.configuration?.apiKey;
+    const organizationId = environment.siteInstallation?.configuration?.organizationId;
+    const deployConfigId = environment.siteInstallation?.configuration?.deployConfigId;
+    const apiKey = environment.siteInstallation?.configuration?.apiKey;
 
-  if (!organizationId || !deployConfigId || !apiKey) {
-    throw new Error(
-      `The Stepsailor organization ID, deploy config ID and API key are missing from the configuration (ID: ${
-        'spaceId' in event ? event.spaceId : event.siteId
-      }).`
-    );
-  }
-
-  return new Response(
-    (script as string)
-      .replace('<TO_REPLACE_ORGANIZATION_ID>', organizationId)
-      .replace('<TO_REPLACE_DEPLOY_CONFIG_ID>', deployConfigId)
-      .replace('<TO_REPLACE_API_KEY>', apiKey),
-    {
-      headers: {
-        'Content-Type': 'application/javascript',
-        'Cache-Control': 'max-age=604800',
-      },
+    if (!organizationId || !deployConfigId || !apiKey) {
+        throw new Error(
+            `The Stepsailor organization ID, deploy config ID and API key are missing from the configuration (ID: ${
+                'spaceId' in event ? event.spaceId : event.siteId
+            }).`,
+        );
     }
-  );
+
+    const scriptContent = (script as string)
+        .replace('<TO_REPLACE_ORGANIZATION_ID>', organizationId)
+        .replace('<TO_REPLACE_DEPLOY_CONFIG_ID>', deployConfigId)
+        .replace('<TO_REPLACE_API_KEY>', apiKey);
+
+    return new Response(scriptContent, {
+        headers: {
+            'Content-Type': 'application/javascript',
+            'Cache-Control': 'max-age=604800',
+        },
+    });
 };
 
 export default createIntegration<StepsailorRuntimeContext>({
-  fetch_published_script: handleFetchEvent,
+    fetch_published_script: handleFetchEvent,
 });
--- integrations/stepsailor/src/script.raw.js
+++ integrations/stepsailor/src/script.raw.js
@@ -1,10 +1,10 @@
 (function (d, s) {
-  d = document;
-  s = d.createElement('script');
-  s.src = 'https://static.api.stepsailor.com/orion-ai/index.js';
-  s.setAttribute('data-organization-id', '<TO_REPLACE_ORGANIZATION_ID>');
-  s.setAttribute('data-deploy-config-id', '<TO_REPLACE_DEPLOY_CONFIG_ID>');
-  s.setAttribute('data-api-key', '<TO_REPLACE_API_KEY>');
-  s.async = 1;
-  d.getElementsByTagName('body')[0].appendChild(s);
+    d = document;
+    s = d.createElement('script');
+    s.src = 'https://static.api.stepsailor.com/orion-ai/index.js';
+    s.setAttribute('data-company-id', '<TO_REPLACE_ORGANIZATION_ID>');
+    s.setAttribute('data-deploy-config-id', '<TO_REPLACE_DEPLOY_CONFIG_ID>');
+    s.setAttribute('data-secret', '<TO_REPLACE_API_KEY>');
+    s.async = 1;
+    d.getElementsByTagName('body')[0].appendChild(s);
 })(window, document);
