16066ab3de6a23f8b5b6ea4800dad866c1471c60
Samy Pessé <samypesse@gmail.com>
2025-04-01 12:36:10 +0200

    Remove openapi

--- integrations/openapi/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { createIntegration } from '@gitbook/runtime';
-
-import { configureComponent } from './components';
-import { generateContentSource } from './contentSources';
-
-export default createIntegration({
-    components: [configureComponent],
-    contentSources: [generateContentSource],
-});
