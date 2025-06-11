693ee24d0e96158cc1f94fac5f5dbc03fb67ff6b
Steven Hall <steven@gitbook.io>
2025-06-11 16:46:32 +0100

    Fix a JS issue with the unify raw script.

--- integrations/unify/src/script.raw.js
+++ integrations/unify/src/script.raw.js
@@ -4,5 +4,5 @@
         n.setAttribute('src', 'https://cdn.unifygtm.com/tag/v1/unify-tag-script.js'),
         n.setAttribute('data-api-key', '<TO_REPLACE>'),
         n.setAttribute('id', 'unifytag'),
-        (document.body | document.head).appendChild(n);
+        (document.body || document.head).appendChild(n);
 })();
