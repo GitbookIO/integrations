2735d6b2bd0fe60bb21e994803152381cefa5a41
KevinR <K.Rohn@outlook.de>
2025-07-02 17:41:14 +0200

    Add salesviewer integration init

---
+++ integrations/salesviewer/src/salesviewerScript.raw.js
@@ -0,0 +1,9 @@
+(function (w, d, s, l, i) {
+    var g = w[l] ? '&s=' + w[l] : '',
+        f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s);
+    j.async = true;
+    j.src = 'https://slsnlytcs.com/stm.js?id=' + i + g;
+    j.referrerPolicy = 'no-referrer-when-downgrade';
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'name', '<TO_REPLACE>');
