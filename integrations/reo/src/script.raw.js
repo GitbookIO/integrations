7eece23402ecb103db7d896fd874b48cb3ff62ec
Addison <addison@gitbook.com>
2025-07-23 16:09:00 +0200

    Update REO

--- integrations/reo/src/script.raw.js
+++ integrations/reo/src/script.raw.js
@@ -1,6 +1,29 @@
 const trackingID = '<TO_REPLACE>';
+const GRANTED_COOKIE = '__gitbook_cookie_granted';
+
+function getCookie(cname) {
+    const name = `${cname}=`;
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return '';
+}
 
 (function (r, e, o) {
+    const disableCookies = getCookie(GRANTED_COOKIE) !== 'yes';
+
+    if (disableCookies) {
+        return;
+    }
+
     var t, c, n;
     c = { clientID: trackingID };
     t = function () {
