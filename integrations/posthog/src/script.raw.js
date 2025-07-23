ea1f7d1e4a207df9915fe68c6e7b807295f98b5a
Addison <addison@gitbook.com>
2025-07-23 16:08:21 +0200

    Update posthog

--- integrations/posthog/src/script.raw.js
+++ integrations/posthog/src/script.raw.js
@@ -1,7 +1,30 @@
 const projectApiKey = '<ph_project_api_key>';
 const instanceAddress = '<ph_instance_address>';
+const GRANTED_COOKIE = '__gitbook_cookie_granted';
+
+function getCookie(cname) {
+    const name = `${cname}=`;
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return '';
+}
 
 !(function (t, e) {
+    const disableCookies = getCookie(GRANTED_COOKIE) !== 'yes';
+
+    if (disableCookies) {
+        return;
+    }
+
     var o, n, p, r;
     e.__SV ||
         ((window.posthog = e),
