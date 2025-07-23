c24fc4891e234ddfd797718ef177cbef8c47bf82
Addison <addison@gitbook.com>
2025-07-23 16:02:18 +0200

    Update fathom script

--- integrations/fathom/src/fathomScript.raw.js
+++ integrations/fathom/src/fathomScript.raw.js
@@ -5,9 +5,28 @@
     element.setAttribute('data-site', siteId);
     element.setAttribute('data-spa', 'auto');
     element.src = 'https://cdn.usefathom.com/script.js';
+    const GRANTED_COOKIE = '__gitbook_cookie_granted';
+
+    function getCookie(cname) {
+        const name = `${cname}=`;
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const ca = decodedCookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return '';
+    }
 
     // Function to track external links
     function trackExternalLink(event) {
+        const disableCookies = getCookie(GRANTED_COOKIE) !== 'yes';
+
         var item = event.target.closest('a');
         if (!item) return;
 
@@ -21,6 +40,10 @@
     }
 
     element.onload = function () {
+        if (disableCookies) {
+            return;
+        }
+
         if (trackExternalLinks === true || trackExternalLinks === 'true') {
             // Use event delegation to capture clicks on all current and later loaded links
             doc.addEventListener('click', trackExternalLink);
