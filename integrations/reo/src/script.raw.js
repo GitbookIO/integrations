aac3f541247022ed0afdeac24533828eccade7ac
Addison <42930383+addisonschultz@users.noreply.github.com>
2025-08-05 16:15:13 +0800

    Check if cookies have been accepted before tracking the page (#915)
    * Update fathom script
    
    * Update heap
    
    * Update hotjar
    
    * Update mixpanel
    
    * Update plausible
    
    * Update posthog
    
    * Update REO
    
    * Add changeset
    
    * Move cookie check out of function call
    
    * Move cookies check to function instead of variable

--- integrations/reo/src/script.raw.js
+++ integrations/reo/src/script.raw.js
@@ -1,6 +1,27 @@
 const trackingID = '<TO_REPLACE>';
+const GRANTED_COOKIE = '__gitbook_cookie_granted';
+
+function getCookie(cname) {
+    const name = `${cname}=`;
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return '';
+}
 
 (function (r, e, o) {
+    if (getCookie(GRANTED_COOKIE) !== 'yes') {
+        return;
+    }
+
     var t, c, n;
     c = { clientID: trackingID };
     t = function () {
