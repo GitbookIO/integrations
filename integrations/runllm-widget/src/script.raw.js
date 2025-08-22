6abb23de0f5ac118c57ab8801798aea18b6b4390
Addison <addison@gitbook.com>
2025-08-22 16:15:43 +0800

    Update load script

--- integrations/runllm-widget/src/script.raw.js
+++ integrations/runllm-widget/src/script.raw.js
@@ -11,55 +11,63 @@
     const disableAskAPerson = '<DISABLE_ASK_A_PERSON>';
     const nativeAiExperience = '<NATIVE_AI_EXPERIENCE>';
 
-    n = document.createElement('script');
-    n.id = 'runllm-widget-script';
-    n.src = 'https://widget.runllm.com';
-    n.async = true;
-    n.type = 'module';
-    n.setAttribute('runllm-assistant-id', assistantID);
-    if (serverAddress) {
-        n.setAttribute('runllm-server-address', serverAddress);
-    }
-    if (name) {
-        n.setAttribute('runllm-name', name);
-    }
-    if (position) {
-        n.setAttribute('runllm-position', position);
-    }
-    if (keyboardShortcut) {
-        n.setAttribute('runllm-keyboard-shortcut', keyboardShortcut);
-    }
-    if (themeColor) {
-        n.setAttribute('runllm-theme-color', themeColor);
-    }
-    if (communityUrl) {
-        n.setAttribute('runllm-community-url', communityUrl);
-    }
-    if (communityType) {
-        n.setAttribute('runllm-community-type', communityType);
-    }
-    if (disableAskAPerson) {
-        n.setAttribute('runllm-disable-ask-a-person', disableAskAPerson);
-    }
-    if (brandLogo) {
-        n.setAttribute('runllm-brand-logo', brandLogo);
-    }
-    if (nativeAiExperience === 'true') {
-        // Disable floating window
-        n.setAttribute('runllm-hide-trigger-button', true);
-    }
+    var l = function () {
+        n = document.createElement('script');
+        n.id = 'runllm-widget-script';
+        n.src = 'https://widget.runllm.com';
+        n.async = true;
+        n.type = 'module';
+        n.setAttribute('runllm-assistant-id', assistantID);
+        if (serverAddress) {
+            n.setAttribute('runllm-server-address', serverAddress);
+        }
+        if (name) {
+            n.setAttribute('runllm-name', name);
+        }
+        if (position) {
+            n.setAttribute('runllm-position', position);
+        }
+        if (keyboardShortcut) {
+            n.setAttribute('runllm-keyboard-shortcut', keyboardShortcut);
+        }
+        if (themeColor) {
+            n.setAttribute('runllm-theme-color', themeColor);
+        }
+        if (communityUrl) {
+            n.setAttribute('runllm-community-url', communityUrl);
+        }
+        if (communityType) {
+            n.setAttribute('runllm-community-type', communityType);
+        }
+        if (disableAskAPerson) {
+            n.setAttribute('runllm-disable-ask-a-person', disableAskAPerson);
+        }
+        if (brandLogo) {
+            n.setAttribute('runllm-brand-logo', brandLogo);
+        }
+        if (nativeAiExperience === 'true') {
+            // Disable floating window
+            n.setAttribute('runllm-hide-trigger-button', true);
+        }
 
-    document.head.appendChild(n);
+        document.head.appendChild(n);
 
-    // Register and open RunLLM Window from GitBook UI
-    window.GitBook.registerAssistant({
-        label: 'RunLLM',
-        icon: 'sparkle',
-        ui: nativeAiExperience === 'true' ? true : false,
-        open: (query) => {
-            // Open RunLLM window with query
-            window.runllm.open();
-            window.runllm.sendMessage(query);
-        },
-    });
+        // Register and open RunLLM Window from GitBook UI
+        window.GitBook.registerAssistant({
+            label: 'RunLLM',
+            icon: 'sparkle',
+            ui: nativeAiExperience === 'true' ? true : false,
+            open: (query) => {
+                // Open RunLLM window with query
+                window.runllm.open();
+                window.runllm.sendMessage(query);
+            },
+        });
+    };
+
+    if (window.attachEvent) {
+        window.attachEvent('onload', l);
+    } else {
+        window.addEventListener('load', l, false);
+    }
 })();
