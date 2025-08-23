80f36c8aace98e26bd228936052e812cebb045f4
Guillermo Calvo <guillermo@guillermo.dev>
2025-08-23 12:45:11 +0200

    Add missing semicolon

--- integrations/ahrefs/src/script.raw.js
+++ integrations/ahrefs/src/script.raw.js
@@ -1,7 +1,7 @@
 (function (win, doc) {
     const trackingID = '<TO_REPLACE>';
     const js = doc.createElement('script');
-    js.type = 'text/javascript'
+    js.type = 'text/javascript';
     js.async = true;
     js.src = 'https://analytics.ahrefs.com/analytics.js';
     js.dataset['key'] = trackingID;
