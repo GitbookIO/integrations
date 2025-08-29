a58129051dab51a87fa35d3a9c188f3717902ed6
Addison <addison@gitbook.com>
2025-08-29 17:36:40 +0800

    Update

--- integrations/zendesk/src/index.tsx
+++ integrations/zendesk/src/index.tsx
@@ -11,7 +11,7 @@ type ZendeskRuntimeContext = RuntimeContext<
     RuntimeEnvironment<
         {},
         {
-            site_tag: string;
+            site_tag?: string;
         }
     >
 >;
@@ -26,7 +26,7 @@ export const handleFetchEvent: FetchPublishScriptEventCallback = async (
         return
     }
 
-    return new Response(script.replace('<TO_REPLACE>', trackingId), {
+    return new Response((script as string).replace('<TO_REPLACE>', trackingId), {
         headers: {
             'Content-Type': 'application/javascript',
             'Cache-Control': 'max-age=604800',
