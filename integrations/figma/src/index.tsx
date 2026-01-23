cdef5e912e5bcda6dd18e2857bf66450cb5faaa0
Addison <42930383+addisonschultz@users.noreply.github.com>
2026-01-23 18:36:25 +0900

    Migrate scope URL (#1104)

--- integrations/figma/src/index.tsx
+++ integrations/figma/src/index.tsx
@@ -180,7 +180,7 @@ export default createIntegration<FigmaRuntimeContext>({
             redirectURL: `${context.environment.integration.urls.publicEndpoint}/oauth`,
             clientId: context.environment.secrets.CLIENT_ID,
             clientSecret: context.environment.secrets.CLIENT_SECRET,
-            authorizeURL: 'https://www.figma.com/oauth?scope=file_read',
+            authorizeURL: 'https://www.figma.com/oauth?scope=file_content:read',
             accessTokenURL: 'https://api.figma.com/v1/oauth/token',
         });
 
