837bd273c4ce11c5aa646c1f60f48384be4016a6
Rostislav Andriciuc <rostislav.andriciuc@collibra.com>
2026-07-07 16:44:13 +0200

    Added cookie-consent gating

--- integrations/piwik/src/piwikScript.raw.js
+++ integrations/piwik/src/piwikScript.raw.js
@@ -1,4 +1,26 @@
 (function(window, document, dataLayerName, id) {
+    var GRANTED_COOKIE = '__gitbook_cookie_granted';
+
+    function getCookie(cname) {
+        var name = cname + '=';
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return '';
+    }
+
+    if (getCookie(GRANTED_COOKIE) !== 'yes') {
+        return;
+    }
+
     window[dataLayerName]=window[dataLayerName]||[],window[dataLayerName].push({start:(new Date).getTime(),event:"stg.start"});var scripts=document.getElementsByTagName('script')[0],tags=document.createElement('script');
     function stgCreateCookie(a,b,c){var d="";if(c){var e=new Date;e.setTime(e.getTime()+24*c*60*60*1e3),d="; expires="+e.toUTCString();f="; SameSite=Strict"}document.cookie=a+"="+b+d+f+"; path=/"}
     var isStgDebug=(window.location.href.match("stg_debug")||document.cookie.match("stg_debug"))&&!window.location.href.match("stg_disable_debug");stgCreateCookie("stg_debug",isStgDebug?1:"",isStgDebug?14:-1);
