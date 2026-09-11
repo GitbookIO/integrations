7bac5dc904e69d6bc7f878eec7ca840aa368be2f
Addison <addison@gitbook.com>
2026-09-11 12:23:46 +0200

    Move posthog variables into script

--- integrations/posthog/src/script.raw.js
+++ integrations/posthog/src/script.raw.js
@@ -1,25 +1,25 @@
-const projectApiKey = '<ph_project_api_key>';
-const apiHost = '<ph_api_host>';
-const uiHost = '<ph_ui_host>';
-const GRANTED_COOKIE = '__gitbook_cookie_granted';
-
-function getCookie(cname) {
-    const name = `${cname}=`;
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) === 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return '';
-}
-
 !(function (t, e) {
+    const projectApiKey = '<ph_project_api_key>';
+    const apiHost = '<ph_api_host>';
+    const uiHost = '<ph_ui_host>';
+    const GRANTED_COOKIE = '__gitbook_cookie_granted';
+
+    function getCookie(cname) {
+        const name = `${cname}=`;
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const ca = decodedCookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return '';
+    }
+
     if (getCookie(GRANTED_COOKIE) !== 'yes') {
         return;
     }
